5ab2bae9cf8fd2331621b731f6a7ef921fe9b633
Mahesh <madisettymahesh052@gmail.com>
2026-06-24 22:39:38 +0530

    Added Terraform backend configuration and tfvars example

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "bucket_name" {
-  value = aws_s3_bucket.storage.bucket
-}
-
 output "instance_public_ip" {
   value = aws_instance.web.public_ip
 }
 
 output "instance_public_dns" {
   value = aws_instance.web.public_dns
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.storage.bucket
 }
